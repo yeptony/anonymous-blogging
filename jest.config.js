42a4e54656a6906696ad4667b6eb2539a4adf7cc
Yeptony <thomasgrosjean00@gmail.com>
2019-02-23 14:19:13 +0100

    mock scss

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/mocks.js',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/mocks.js'
+      '<rootDir>/__mocks__/fileMocks.js',
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMocks.js'
   }
 }
